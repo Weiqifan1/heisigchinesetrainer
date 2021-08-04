515a4a96ea11219849f6bc9c51e7d230aa70795a
Weiqifan1 <cmlykke@hotmail.com>
2021-08-04 18:39:59 +0200

    2021-08-04-1839_mislykketForsoegMedEnKnapTilAtSkifteDisplayTegn

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import TrainHeisig from './components/TrainHeisig';
 
 function App() {
   const heisigCorpus = [
-    {traditionalCorpus: true, number: 1, character: '我', word: 'I,me', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'},
-    {traditionalCorpus: true, number: 2, character: '你', word: 'you', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'},
-    {traditionalCorpus: true, number: 3, character: '他', word: 'he', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'},
-    {traditionalCorpus: true, number: 4, character: '她', word: 'she', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'},
-    {traditionalCorpus: true, number: 5, character: '它', word: 'it', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'},
-    {traditionalCorpus: true, number: 6, character: '們', word: 'plural', repetitionNumber: 0, memo: 'her er den text man skal huske tegnet paa'}
+    {traditionalCorpus: true, number: 1, character: '我', word: 'I,me', repetitionNumber: 8, memo: 'her er den text man skal huske tegnet paa'},
+    {traditionalCorpus: true, number: 2, character: '你', word: 'you', repetitionNumber: 7, memo: 'her er den text man skal huske tegnet paa'},
+    {traditionalCorpus: true, number: 3, character: '他', word: 'he', repetitionNumber: 5, memo: 'her er den text man skal huske tegnet paa'},
+    {traditionalCorpus: true, number: 4, character: '她', word: 'she', repetitionNumber: 5, memo: 'her er den text man skal huske tegnet paa'},
+    {traditionalCorpus: true, number: 5, character: '它', word: 'it', repetitionNumber: 4, memo: 'her er den text man skal huske tegnet paa'},
+    {traditionalCorpus: true, number: 6, character: '們', word: 'plural', repetitionNumber: 3, memo: 'her er den text man skal huske tegnet paa'}
     
   ];
   return (  
--- src/components/TrainHeisig.js
+++ src/components/TrainHeisig.js
@@ -1,9 +1,45 @@
 import HeisigEntryInfo from './HeisigEntryInfo';
 
 function TrainHeisig(props){
+
+    //returns the appropriate heisig item to be displayed
+    function getHeisigItemToDisplay(heisigObjects){
+        //to begin with, simply choose the character with the lowest number
+        //eg. lowest repetition first, then lowest heisig number
+        //////first first, choose one at random
+        var lengthOfCorpus = heisigObjects.length;
+        if (lengthOfCorpus < 1){
+            return null;
+        }
+        var indexToChoose = getRandomInt(0, lengthOfCorpus-1);
+        var heisigItemToReturn = heisigObjects[indexToChoose];
+        return heisigItemToReturn;
+    }
+
+    /**
+     * Returns a random integer between min (inclusive) and max (inclusive).
+     * The value is no lower than min (or the next integer greater than min
+     * if min isn't an integer) and no greater than max (or the next integer
+     * lower than max if max isn't an integer).
+     * Using Math.round() will give you a non-uniform distribution!
+     */
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    var currentHeisig = props.heisigCorpus[0];
+
+    function assignToHeisig(heisigObjects){
+        var itemToSave = getHeisigItemToDisplay(props.heisigCorpus);
+        currentHeisig = itemToSave;
+    }
+
     return <div> 
-        <p> her er TrainHeisig</p>
-        <HeisigEntryInfo heisigItem={props.heisigCorpus[0]}> </HeisigEntryInfo>
+        <p> her er trainHeisig</p>
+        <button onClick={assignToHeisig(props.heisigCorpus)}> newHeisig </button>
+        <HeisigEntryInfo heisigItem={currentHeisig}> </HeisigEntryInfo>
     </div>
 }
 
