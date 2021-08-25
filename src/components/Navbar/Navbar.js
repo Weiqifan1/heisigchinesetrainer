003dcd4338c46d4a835ab1a8df04508f63a4489b
Weiqifan1 <cmlykke@hotmail.com>
2021-08-25 20:23:09 +0200

    2021-08-25： changed navBar to functional component (and removed state)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems";
 import './Navbar.css';
 
-class Navbar extends Component {
-    state = {clicked: false}
+const Navbar = props => {
 
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
+    const handleClick = (e) => {
+        console.log('you clicked the button!');
     }
 
-    render() {
         return(
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                <h1 className="navbar-logo">React</h1>
+                <div className="menu-icon">
+
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active' :
-                    'nav-menu'}>
+                <ul className=
+                    'nav-menu'>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <a className={item.cName} href={item.url}>
+                                <a className={item.cName} href={item.url} onClick={handleClick}>
                                     {item.title}
                                 </a>
                             </li>
@@ -29,8 +27,8 @@ class Navbar extends Component {
                     })}
                 </ul>
             </nav>
-        )
-    }
+        );
+
 }
 
 export default Navbar;
